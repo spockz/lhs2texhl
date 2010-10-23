f3ea56fca207691fce60f60b361b27c79a992015
Alessandro Vermeulen <avermeulen@spockz.nl>
2010-10-24 01:11:26 +0200

    * Now with command line arguments lib * And more hackage compliant

--- LiterateHighlighter.hs
+++ LiterateHighlighter.hs
@@ -10,6 +10,9 @@ import Language.Haskell.Exts
 
 -----------------------------
 
+import Base.CLI
+import System.Console.CmdArgs
+
 import Literate.Agda
 import Literate.Haskell 
 import Literate.SimpleInfo
@@ -17,29 +20,28 @@ import Literate.SimpleInfo
 -----------------------------
 
 
-main = do args <- getArgs
-          if length args < 2
-            then
-              putStrLn "Please provide two arguments exec INPUT OUTPUT"
-            else
-              do 
-                 hSetEncoding stdin  utf8
-                 hSetEncoding stdout utf8
-                 hSetEncoding stderr utf8
-                 
-                 output <- openFile  (head $ tail args) WriteMode
-                 hSetEncoding output utf8
+main = do args <- cmdArgsRun standard
 
-                 let writer = writeOutput output
-                                  
-                 if (elem "--agda" args)
-                   then
-                     do si <- runAgda (head args)
-                        writer si Literate.Agda.mapping
-                   else
-                     do si <- runHaskell (head args)
-                        writer si Literate.Haskell.mapping
-                 hClose output
+          hSetEncoding stdin  utf8
+          hSetEncoding stdout utf8
+          hSetEncoding stderr utf8
+         
+          hOutput <- openFile  (output args) WriteMode
+          hSetEncoding hOutput utf8
+
+          let writer = writeOutput hOutput
+                           
+          if (agda_mode args)
+            then
+              mapM_ (\file -> runAgda file
+                     >>= (flip writer) Literate.Agda.mapping)
+                    (input args)
+            else
+              mapM_ (\file -> runHaskell file
+                     >>= (flip writer) Literate.Haskell.mapping)
+                    (input args)
+          
+          hClose hOutput
   where printFormat keyword seek rep = "%format " ++ seek ++ " = \" {\\lhsCH" ++ keyword ++ "{" ++ rep ++ "}}\"" 
         writeOutput output si mapping = 
                       mapM_ (\(keyword, f) -> mapM_ (\ (seek,rep) -> hPutStrLn output $ printFormat keyword seek rep) 
