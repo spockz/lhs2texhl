eb56750b26ea67b5f1af6bbb3798ad72b6fe37af
Alessandro Vermeulen <avermeulen@spockz.nl>
2010-11-01 17:17:38 +0100

    * Added missing files * Bit of a cleanup

---
+++ src/Base/Cli.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+module Base.CLI (ProgramOptions(..), usage, standard, module System.Console.CmdArgs) where
+  
+import System.Console.CmdArgs
+
+import Base.Common
+
+data ProgramOptions = ProgramOptions
+  { agda_mode :: Bool
+  , input   :: [FilePath]
+  , output  :: FilePath
+  } deriving (Show, Data, Typeable)
+
+
+
+usage :: String
+usage = unlines 
+          [ programName ++" "++ programVersion ++" - A lhs2TeX Syntax Coloring pre-processor"
+          , "Copyright 2010, Alessandro Vermeulen <me@alessandrovermeulen.me>" ]
+
+
+
+-- | Standard command line options. 
+--
+
+standard = cmdArgsMode $ ProgramOptions 
+            { 
+              agda_mode  = def &= help "Run in agda-mode!"  
+            , output     = (def &= help "Output file") &= typFile
+            , input      = (def &= args ) 
+            } &= summary usage
+
+
---
+++ src/Base/Common.hs
@@ -0,0 +1,4 @@
+module Base.Common where
+  
+programVersion = "0.1.1.0"
+programName    = "lhs2TeX-hl"
